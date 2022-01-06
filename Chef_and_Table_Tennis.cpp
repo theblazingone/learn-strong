105bdf8173233f4a27ca9d0cd5c811b8bc4388a5
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-01-06 19:14:40 +0530

    Add files via upload

---
+++ Chef_and_Table_Tennis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while (t--){
+
+        string s;
+        cin>>s;
+
+        int win_p=0, lose_p=0;
+
+        puts ( s[s.length()-1] == '1' ? "WIN" : "LOSE");
+    }
+
+    return 0;
+}
