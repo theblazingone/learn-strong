d751ebd8a46ad7e786f3ae3a588c78f3e2a065b9
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-01-09 18:36:31 +0530

    Add files via upload

---
+++ Find_the_Maximum_Value.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while (t--){
+
+        int n1, n2;
+        cin>>n1, n2;
+
+        for (int i=0; i<n1 || i<n2; i++){
+            if (A[i])
+        }
+    }
+
+    return 0;
+}
