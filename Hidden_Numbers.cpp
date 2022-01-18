c9f8ac315b03ee42a3e1491c92556ea4401d8bc4
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-01-18 18:56:09 +0530

    Add files via upload

---
+++ Hidden_Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while (t--){
+
+        int num;
+        cin>>num;
+        cout<<"1"<<" "<<num<<endl;
+    }
+
+    return 0;
+}
