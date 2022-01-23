09218f9a51cb2ad1d8683121b5e297c5a2946b07
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-01-23 22:42:42 +0530

    Add files via upload

---
+++ Coder_Life_Matters.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while (t--){
+
+        int A[30], flag=0;
+
+        for (int i=0; i<30; i++){
+            cin>>A[i];
+        }
+
+        // optimised solution
+
+        // int cnt=0;
+        // for (int i=0; i<30; i++){
+        //     if (A[i] == 1){
+        //         cnt++;
+        //     }
+        //     else {
+        //         if (cnt>5){
+        //             flag = 1;
+        //             break;
+        //         }
+        //         cnt=0;
+        //     }
+        // }
+
+        
+
+        // brute force
+
+        for (int i=0; i<30; i++){
+            if (A[i] == 1 && A[i+1] == 1 && A[i+2] == 1 && A[i+3] == 1 && A[i+4] == 1 && A[i+5] == 1){
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag){
+            cout<<"#coderlifematters"<<endl;
+        }
+        else {
+            cout<<"#allcodersarefun"<<endl;
+        }
+    }
+
+    return 0;
+}
